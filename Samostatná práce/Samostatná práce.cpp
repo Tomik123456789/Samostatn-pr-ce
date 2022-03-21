539f33fd7a2b3832d8191f945d0538516b987e2d
Tomáš Cvrček1 <tomas.cvrcek.120@pslib.cz>
2022-03-21 13:30:58 +0100

    Přidejte soubory projektu.

---
+++ Samostatná práce/Samostatná práce.cpp
@@ -0,0 +1,23 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+    int cislo = 0;
+    int suda = 0;
+    int licha = 0;
+    printf("zadejte libovolne cislo: \n");
+    scanf("%d", &cislo);
+    for (int i = 1; i <= cislo; i++) {
+        if (i % 2 == 0) {
+            suda += i;
+        }
+        else (i % 2 != 0); {
+            licha += i;
+        }
+    }
+    printf("soucet sudych cisel je: %d\n", suda);
+    printf("soucet lichych cisel je: %d\n", licha);
+}
+
